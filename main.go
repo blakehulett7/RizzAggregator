fa8d20420ec41e90525e42430a8cd4dac3c2f4af
blakehulett7 <blake.hulett7@gmail.com>
2024-08-15 20:09:27 -0500

    Env, mux, and server added

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    "localhost:" + os.Getenv("PORT"),
+		Handler: mux,
+	}
 	fmt.Println("Christ is King!")
 }
