fcec31c17b3893a436d0f4392ad573c38133a9ea
blakehulett7 <blake.hulett7@gmail.com>
2024-08-16 12:23:07 -0500

    Sqling hard, lots of packages and encapsulation here i don't like that...

--- handlers.go
+++ handlers.go
@@ -3,7 +3,13 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/blakehulett7/RizzAggregator/internal/database"
 )
+
+type apiConfig struct {
+	Database *database.Queries
+}
 
 func ReportHealth(writer http.ResponseWriter, request *http.Request) {
 	responseStruct := struct {
@@ -13,4 +19,8 @@ func ReportHealth(writer http.ResponseWriter, request *http.Request) {
 	}
 	responseData, _ := json.Marshal(responseStruct)
 	JsonResponse(writer, 200, responseData)
+}
+
+func (config apiConfig) AddUser() {
+
 }
--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/joho/godotenv"
 	"net/http"
 	"os"
+
+	"github.com/blakehulett7/RizzAggregator/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	godotenv.Load()
+	dbURL := os.Getenv("CONN")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Errorf("Error opening database", err)
+	}
+	dbQueries := database.New(db)
+	config := apiConfig{
+		dbQueries,
+	}
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    "localhost:" + os.Getenv("PORT"),
@@ -16,6 +29,7 @@ func main() {
 	}
 	fmt.Println("Christ is King!, also the server is starting...")
 	mux.HandleFunc("GET /v1/healthz", ReportHealth)
+	mux.HandleFunc("POST /v1/healthz", config.AddUser)
 	server.ListenAndServe()
 }
 
